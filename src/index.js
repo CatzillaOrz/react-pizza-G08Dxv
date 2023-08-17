98ec4e850beebffba47c821cf13475a0623828e8
catzilla <catzillaorz@outlook.com>
2023-08-17 12:02:43 +0800

    feat: React 18 vs 17 | react 18 StrictMode

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ function App() {
   return <h1>Hello React!</h1>
 }
 
-const root  = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+ * StrictMode will render components twice 
+ * in order to : 
+ * 1. to find any bugs in components
+ * 2. to check if deprecated methods has been used
+ */
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-root.render(<App/>);
+/*
+ * react 17 would be like
+ * import ReactDOM from 'react-dom';
+ * React.rander(<App/>);
+ */
